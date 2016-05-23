827b0dcad01f54aff9dab1e0fed3f81dbc170a5e
Janis <janis.chinn@praece.com>
2016-05-23 13:20:32 -0700

    Add individual cell editing on click.

--- tracker.js
+++ tracker.js
@@ -5,31 +5,11 @@ var button = document.querySelectorAll('button');
 var inputs = document.querySelectorAll('.inputs input');
 var upload = document.querySelector('.upload input');
 var exportData = []; 
-
-upload.addEventListener('change', function() {
-	Array.prototype.forEach.call(upload.files, function(file) {
-		readFile(file).then(function(data) {
-			table.textContent = '';
-			var json = JSON.parse(data);
-			for (var i = 0; i < json.length; i++) {
-				var row = document.createElement('tr');
-				row.appendChild(elt('td', json[i].todo));
-				row.appendChild(elt('td', json[i].need));
-				row.appendChild(elt('td', json[i].have));
-				row.appendChild(elt('td', json[i].where));
-				
-				deleteButton(row);
-				table.appendChild(row);
-			}
-			upload.value = '';
-		}, function(error) {
-			console.log("Couldn't import data.");
-		});
-
-	});
-});
-
 var added = [];
+
+
+
+
 button[0].addEventListener('click', submit);
 for (var i = 0; i < inputs.length; i++) {
 	inputs[i].addEventListener('keypress', function(event) {
@@ -39,6 +19,7 @@ for (var i = 0; i < inputs.length; i++) {
 	});
 }
 
+//import
 function readFile(file) {
 	return new Promise(function(succeed, fail) {
 		var reader = new FileReader();
@@ -53,16 +34,43 @@ function readFile(file) {
 
 }
 
-function elt(name, text) {
+function elt(name, text, eventAction, fun) {
 	var el = document.createElement(name);
 	el.textContent = text;
+	el.addEventListener(eventAction, function(event){
+		fun(event);
+	});
 	return el;
 }
 
+upload.addEventListener('change', function() {
+	Array.prototype.forEach.call(upload.files, function(file) {
+		readFile(file).then(function(data) {
+			table.textContent = '';
+			added = [];
+			exportData = [];
+			var json = JSON.parse(data);
+			for (var i = 0; i < json.length; i++) {
+				var row = document.createElement('tr');
+				row.appendChild(elt('td', json[i].todo, 'click', editInline));
+				row.appendChild(elt('td', json[i].need, 'click', editInline));
+				row.appendChild(elt('td', json[i].have, 'click', editInline));
+				row.appendChild(elt('td', json[i].where, 'click', editInline));
+				added.push(json[i].todo);
+				deleteButton(row);
+				table.appendChild(row);
+			}
+			upload.value = '';
+		}, function(error) {
+			console.log("Couldn't import data.");
+		});
 
+	});
+});
+
+//export
 button[1].addEventListener('click', function() {
 	var rows = document.querySelectorAll('tr:not(#headings)');
-	console.log(rows);
 	Array.prototype.forEach.call(rows, function(row) {
 		var todo = row.children[0].textContent;
 		var need = row.children[1].textContent;
@@ -75,10 +83,10 @@ button[1].addEventListener('click', function() {
 			'where': where
 		});
 	});
-	console.log(exportData);
 	var url = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(exportData));
 	window.open(url, '_blank');
 	window.focus();
+	exportData = [];
 });
 
 function submit() {
@@ -110,9 +118,50 @@ function createRow(row) {
 			var cell = document.createElement('td');
 			cell.textContent = inputs[i].value;
 			row.appendChild(cell);
+			cell.addEventListener('click', function(event) {
+				editInline(event);
+			});
 			inputs[i].value = '';
 	}
 	inputs[0].focus();
+}
+
+function change(edit, cell) {
+	var replace = edit.value;
+	if (cell.previousSibling == null) {
+		added.splice(added.indexOf(cell.textContent));
+		added.push(replace);
+	}
+	
+	cell.textContent = replace;
+}
+
+function editInline(e) {
+	var edit = document.createElement('input');
+	var cell = e.target;
+	edit.addEventListener('blur', function() {
+		change(edit, cell)
+	});
+	edit.addEventListener('keypress', function(event){
+		if (event.keyCode == 13) {
+			change(edit, cell);
+		}
+	});
+	// 	, function() {
+	// 	var replace = edit.value;
+	// 	added.splice(added.indexOf(cell.textContent),1);
+	// 	cell.textContent = replace;
+	// 	added.push(replace);
+	// 	console.log('after: ' + added);
+	// })
+	edit.type = 'text';
+	edit.className = 'edit';
+	var text = e.target.textContent;
+	e.target.textContent = '';
+	edit.value = text;
+	e.target.appendChild(edit);
+	edit.focus();
+	e.stopPropagation();
 }
 
 function updateRow(row, old) {
@@ -129,6 +178,9 @@ function updateRow(row, old) {
 			cell.textContent = inputs[i].value;
 		}
 		row.appendChild(cell);
+		cell.addEventListener('click', function(event) {
+			editInline(event);
+	});
 		inputs[i].value = '';
 	}
 	inputs[0].focus()
